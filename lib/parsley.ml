37fdda449274dfdd4f6a3b78e70d3e7fe0935905
j03m4r <joeemarr12345@gmail.com>
2023-11-10 09:53:56 -0600

    The parser, lexer, and main (driver) can parse and print back function application)

--- lib/parsley.ml
+++ lib/parsley.ml
@@ -13,4 +13,24 @@ let parse (s : string) : expression list =
   | Constructor (name, patterns) -> name ^ " (" ^ (String.concat ", " (List.map string_of_pattern patterns)) ^ ")"
   ... of course, this part will depend on what your datatypes look like. *)
 
-let string_of_declaration _p = "p"
+let rec string_of_declaration (p : expression) =
+  match p with
+    | Variable x -> x
+    | Application ((func : expression), (var : expression)) -> string_of_declaration func ^ " (" ^ string_of_declaration var ^ ")"
+    | Tuple (vars : expression list) -> begin
+      let rec string_of_exprs (exprs : expression list) = 
+        match exprs with
+        | [] -> ""
+        | h::tl -> string_of_declaration h ^ string_of_exprs tl
+      in string_of_exprs vars
+    end
+  (* match p with
+  | [] -> "\n"
+  | (h : expression)::tl -> begin
+    match h with
+    | Variable x -> x
+    | Application ((func : expression), (var : expression)) -> string_of_declaration [func] ^ " (" ^ string_of_declaration [var] ^ ")"
+    | Tuple (vars : expression list) -> "(" ^ string_of_declaration vars ^ ")"
+    ^ "\n" ^
+    string_of_declaration tl
+  end *)

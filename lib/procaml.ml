c9ccf81fd0b787f7534b056b2a0c55e62dfd04a3
j03m4r <joeemarr12345@gmail.com>
2023-12-01 10:09:47 -0600

    working vers.

--- lib/procaml.ml
+++ lib/procaml.ml
@@ -124,6 +124,8 @@ let rec mkConstructorApp expr variantvars = match variantvars with
     Application (expr, mkConstructorApp (Application (Identifier ",", Identifier var)) rest)
 
 let caseproof vars varnm typenm rules lhs rhs (variantnm, _variantvars) =
+  let _ = List.map (fun x -> x) vars in
+  print_endline typenm;
   let variant_namedvars = [("?1","int");("?2","list")] (* TODO: construct this from variantvars *) in
     let variant_expr = mkConstructorApp (Identifier variantnm) variant_namedvars in
     let caserule = ([], "case", Identifier varnm, variant_expr) in
@@ -133,10 +135,10 @@ let caseproof vars varnm typenm rules lhs rhs (variantnm, _variantvars) =
     List.map (fun (vars, nm, lhs, rhs) -> nm^": "^String.concat "" (List.map (fun x -> "Forall "^x^". ") vars)^String_of.string_of_expression lhs^" = "^String_of.string_of_expression rhs) ihs @
     prove variantrules lhs rhs @ ["This completes the proof of case "^ variantnm ^ ".";""]
 
-let inductionproof varnm _types vars rules lhs rhs =
+let inductionproof proof_name varnm _types vars rules lhs rhs =
   let typenm = "list" (* TODO: don't hard-code the type! *) in
   let variants = [("Cons",["int";"list"]) ; ("Nil" , [])] (* TODO: find the type in 'types' and convert it to this! *) in
-  ("Proof by induction on (" ^ varnm ^ " : "^ typenm^") :") ::
+  ("Proof " ^ proof_name ^ " by induction on (" ^ varnm ^ " : " ^ typenm^"):") ::
   List.(concat (map (caseproof vars varnm typenm rules lhs rhs) variants)) @
   ["This completes the proof by induction."]
   
@@ -149,9 +151,12 @@ let rec prover types rules declarations =
             -> (* we got a hint so we simply assume the statement *)
               (match hint with 
               | Axiom -> prover types ((List.map typedVariableVariable vars,nm,lhs,rhs)::rules) rest
-              | Induction x -> inductionproof x types vars rules lhs rhs :: prover types ((List.map typedVariableVariable vars,nm,lhs,rhs)::rules) rest)
-         | _ :: rest -> prover types rules rest
-         | [] -> []
+              | Induction x -> inductionproof nm x types vars rules lhs rhs :: prover types ((List.map typedVariableVariable vars,nm,lhs,rhs)::rules) rest)
+        | TypeDeclaration (nm, variants) :: rest -> (* add the type to the list of types and keep going *)
+          prover ((nm, variants)::types) rules rest
+        | FunctionDeclaration _ :: rest -> prover types rules rest
+        | [] -> []
+
 let prover_main decls =
   prover [] [] decls |>
   List.map (String.concat "\n") |>

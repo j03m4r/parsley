decdab5628ce18f6f5536b0d26a71edfb962a9ee
j03m4r <joeemarr12345@gmail.com>
2023-12-01 09:51:22 -0600

    Induction template

--- lib/procaml.ml
+++ lib/procaml.ml
@@ -116,21 +116,44 @@ let prove rules lhs rhs
       lockstep lhs rhs' lhs_steps rhs_steps' @
       List.rev (print_steps_n rhs rhs_steps (rhs_n - lhs_n))
       )
+
+let rec mkConstructorApp expr variantvars = match variantvars with
+  | [] -> expr
+  | [var, _] -> Application (expr, Identifier var)
+  | (var, _)::rest ->
+    Application (expr, mkConstructorApp (Application (Identifier ",", Identifier var)) rest)
+
+let caseproof vars varnm typenm rules lhs rhs (variantnm, _variantvars) =
+  let variant_namedvars = [("?1","int");("?2","list")] (* TODO: construct this from variantvars *) in
+    let variant_expr = mkConstructorApp (Identifier variantnm) variant_namedvars in
+    let caserule = ([], "case", Identifier varnm, variant_expr) in
+  let ihs = [] (* TODO: generate the inductive hypotheses for all variant_namedvars that are of type typenm *) in
+    let variantrules = caserule::ihs @ rules in
+    ("Case "^ variantnm ^ ":") ::
+    List.map (fun (vars, nm, lhs, rhs) -> nm^": "^String.concat "" (List.map (fun x -> "Forall "^x^". ") vars)^String_of.string_of_expression lhs^" = "^String_of.string_of_expression rhs) ihs @
+    prove variantrules lhs rhs @ ["This completes the proof of case "^ variantnm ^ ".";""]
+
+let inductionproof varnm _types vars rules lhs rhs =
+  let typenm = "list" (* TODO: don't hard-code the type! *) in
+  let variants = [("Cons",["int";"list"]) ; ("Nil" , [])] (* TODO: find the type in 'types' and convert it to this! *) in
+  ("Proof by induction on (" ^ varnm ^ " : "^ typenm^") :") ::
+  List.(concat (map (caseproof vars varnm typenm rules lhs rhs) variants)) @
+  ["This completes the proof by induction."]
   
-let rec prover rules declarations =
+let rec prover types rules declarations =
       match declarations with
          | ProofDeclaration (nm, vars, Equality (lhs,rhs), None) :: rest
             -> (* no hint, so let's prove this *)
-               prove rules lhs rhs :: prover ((List.map typedVariableVariable vars,nm,lhs,rhs)::rules) rest
+               prove rules lhs rhs :: prover types ((List.map typedVariableVariable vars,nm,lhs,rhs)::rules) rest
          | ProofDeclaration (nm, vars, Equality (lhs,rhs), Some hint) :: rest
             -> (* we got a hint so we simply assume the statement *)
               (match hint with 
-              | Axiom -> prover ((List.map typedVariableVariable vars,nm,lhs,rhs)::rules) rest
-              | Induction x -> [string_of_expression lhs; "= { induction " ^ x ^ " }"; string_of_expression rhs] :: prover ((["idk"], "big induction", Identifier "lhs", Identifier "rhs") :: rules) rest)
-         | _ :: rest -> prover rules rest
+              | Axiom -> prover types ((List.map typedVariableVariable vars,nm,lhs,rhs)::rules) rest
+              | Induction x -> inductionproof x types vars rules lhs rhs :: prover types ((List.map typedVariableVariable vars,nm,lhs,rhs)::rules) rest)
+         | _ :: rest -> prover types rules rest
          | [] -> []
-   let prover_main decls =
-      prover [] decls |>
-      List.map (String.concat "\n") |>
-      String.concat "\n\n" |>
-      print_endline
+let prover_main decls =
+  prover [] [] decls |>
+  List.map (String.concat "\n") |>
+  String.concat "\n\n" |>
+  print_endline
